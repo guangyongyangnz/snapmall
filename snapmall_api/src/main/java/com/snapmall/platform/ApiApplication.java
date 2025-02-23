5b7d5d670569fe00cc054d40c7c205f6f4c37e74
guangyong.yang <chunming.ygy@gmail.com>
2025-02-23 17:14:20 +1300

    fix log issue

--- snapmall_api/src/main/java/com/snapmall/platform/ApiApplication.java
+++ snapmall_api/src/main/java/com/snapmall/platform/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.snapmall.platform;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.snapmall.platform.dao")
+@Slf4j
 public class ApiApplication {
     public static void main(String[] args) {
+        log.info("ApiApplication Starting.....");
         SpringApplication.run(ApiApplication.class, args);
     }
 }
--- snapmall_service/src/main/java/com/snapmall/platform/service/impl/HelloWorldServiceImpl.java
+++ snapmall_service/src/main/java/com/snapmall/platform/service/impl/HelloWorldServiceImpl.java
@@ -2,12 +2,14 @@ package com.snapmall.platform.service.impl;
 
 import com.snapmall.platform.dao.HelloWorldDAO;
 import com.snapmall.platform.service.HelloWorldService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * @author chunming.ygy@gmail.com
  * @date 21/02/2025 22:28
  */
+@Slf4j
 @Service
 public class HelloWorldServiceImpl implements HelloWorldService {
 
@@ -20,6 +22,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
 
     @Override
     public String hello() {
+        log.info("hello");
         return helloWorldDAO.queryList().stream().findFirst().get().getName();
     }
 }
